d2f8633f41f49347119b3aaada94ab3dce986fcf
unknown <cameronvoell@gmail.com>
2019-02-05 01:25:27 -0800

    Added code for fabNavigatesToEditDraft. now passing as we correctly navigate to EditDraftActivity

--- ArticleDraftManager/app/src/main/java/com/cameronvoell/articledraftmanager/activities/MainActivity.java
+++ ArticleDraftManager/app/src/main/java/com/cameronvoell/articledraftmanager/activities/MainActivity.java
@@ -3,7 +3,6 @@ package com.cameronvoell.articledraftmanager.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -27,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent();
                 intent.setClass(getApplicationContext(), EditDraftActivity.class);
-                //startActivity(intent);
+                startActivity(intent);
             }
         });
     }
